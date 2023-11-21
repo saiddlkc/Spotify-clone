b690a4dad534f589934eefecfd0bdfb230d45a79
saiddlkc <saidalkilic@gmail.com>
2023-11-21 15:56:28 +0100

    added api in search function

--- javascript/searchpage.js
+++ javascript/searchpage.js
@@ -1,21 +1,73 @@
 const input = document.getElementById("suche");
 console.log(input);
-async;
 
-input.addEventListener("change");
-const url = `https://deezerdevs-deezer.p.rapidapi.com/search?q=`;
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "55af72b14emsh898860a4d9a021dp18af58jsn88df01865c54",
-    "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
-  },
-};
+// Function to handle both change and Enter key press
+async function fetchData(searchArea) {
+  try {
+    const url = `https://deezerdevs-deezer.p.rapidapi.com/search?q=${searchArea}`;
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "55af72b14emsh898860a4d9a021dp18af58jsn88df01865c54",
+        "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+      },
+    };
 
-try {
-  const response = await fetch(url, options);
-  const result = await response.text();
-  console.log(result);
-} catch (error) {
-  console.error(error);
+    const response = await fetch(url, options);
+    const data = await response.json();
+    console.log(data);
+    document.getElementById("ergebnisse").innerHTML = "";
+
+    data.data.forEach((song) => {
+      const container = document.getElementById("ergebnisse");
+      const miniContainer = document.createElement("DIV");
+      const myH = document.createElement("H4");
+      const myP = document.createElement("P");
+      const myImg = document.createElement("IMG");
+      const br = document.createElement("br");
+
+      myH.innerText = song.title;
+      myP.innerText = song.artist.name;
+      myImg.setAttribute("src", song.album.cover_medium);
+      console.log(myH);
+      console.log(myP);
+
+      myH.classList.add("d-flex");
+      myH.classList.add("fontsize");
+      myH.classList.add("flex-wrap");
+      myH.classList.add("ms-2");
+      myH.classList.add("align-items-end");
+      myP.classList.add("ms-2");
+      //   myImg.classList.add("");
+      myImg.classList.add("img-small");
+      miniContainer.appendChild(myImg);
+      miniContainer.appendChild(myP);
+      miniContainer.appendChild(br);
+      miniContainer.appendChild(myH);
+      miniContainer.classList.add("border");
+      miniContainer.classList.add("border-subtle");
+      miniContainer.classList.add("fontsize");
+      miniContainer.classList.add("fixlength");
+
+      container.appendChild(miniContainer);
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
+
+input.addEventListener("change", function (e) {
+  e.preventDefault();
+  const searchArea = input.value;
+  console.log(searchArea);
+  fetchData(searchArea);
+});
+
+input.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    const searchArea = input.value;
+    console.log(searchArea);
+    fetchData(searchArea);
+  }
+});
