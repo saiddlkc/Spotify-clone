1ae42fbb0f934bc498fbb7580f29a9d35471a43b
Tairou Mouhamed <tairou97-mouhamed@hotmail.com>
2023-11-29 11:03:42 +0100

    Merge branch 'Said' into Tairou

---
+++ javascript/index.js
@@ -0,0 +1,131 @@
+// const input = document.getElementById("search-artist");
+// console.log(input);
+// const button = document.getElementById("search-button");
+// const view = document.getElementById("artist-biew")
+// console.log(view);
+// let searchArtist = " ";
+
+// let array = [
+//     {
+//         name: "Rock",
+//         id: 152,
+//         picture: "https://api.deezer.com/genre/152/image",
+//         picture_small: "https://e-cdns-images.dzcdn.net/images/misc/b36ca681666d617edd0dcb5ab389a6ac/56x56-000000-80-0-0.jpg",
+//         picture_medium: "https://e-cdns-images.dzcdn.net/images/misc/b36ca681666d617edd0dcb5ab389a6ac/250x250-000000-80-0-0.jpg",
+//         picture_big: "https://e-cdns-images.dzcdn.net/images/misc/b36ca681666d617edd0dcb5ab389a6ac/500x500-000000-80-0-0.jpg",
+//         picture_xl: "https://e-cdns-images.dzcdn.net/images/misc/b36ca681666d617edd0dcb5ab389a6ac/1000x1000-000000-80-0-0.jpg",
+//         type: "genre"
+//     },
+//     {
+//         name: "Klassik",
+//         id: 98,
+//         picture: "https://api.deezer.com/genre/98/image",
+//         picture_small: "https://e-cdns-images.dzcdn.net/images/misc/609f69b669b242252aa8ee09b5597655/56x56-000000-80-0-0.jpg",
+//         picture_medium: "https://e-cdns-images.dzcdn.net/images/misc/609f69b669b242252aa8ee09b5597655/250x250-000000-80-0-0.jpg",
+//         picture_big: "https://e-cdns-images.dzcdn.net/images/misc/609f69b669b242252aa8ee09b5597655/500x500-000000-80-0-0.jpg",
+//         picture_xl: "https://e-cdns-images.dzcdn.net/images/misc/609f69b669b242252aa8ee09b5597655/1000x1000-000000-80-0-0.jpg",
+//         type: "genre"
+//     },
+//     {
+//         name: "Soul & Funk",
+//         id: 169,
+//         picture: "https://api.deezer.com/genre/169/image",
+//         picture_small: "https://e-cdns-images.dzcdn.net/images/misc/3d5e8aab99b95bfa7ac7e9e466e7781e/56x56-000000-80-0-0.jpg",
+//         picture_medium: "https://e-cdns-images.dzcdn.net/images/misc/3d5e8aab99b95bfa7ac7e9e466e7781e/250x250-000000-80-0-0.jpg",
+//         picture_big: "https://e-cdns-images.dzcdn.net/images/misc/3d5e8aab99b95bfa7ac7e9e466e7781e/500x500-000000-80-0-0.jpg",
+//         picture_xl: "https://e-cdns-images.dzcdn.net/images/misc/3d5e8aab99b95bfa7ac7e9e466e7781e/1000x1000-000000-80-0-0.jpg",
+//         type: "genre"
+//     },
+//     {
+//         name: "Country",
+//         id: 84,
+//         picture: "https://api.deezer.com/genre/84/image",
+//         picture_small: "https://e-cdns-images.dzcdn.net/images/misc/6eca3188f724f04843a15e3e575751a5/56x56-000000-80-0-0.jpg",
+//         picture_medium: "https://e-cdns-images.dzcdn.net/images/misc/6eca3188f724f04843a15e3e575751a5/250x250-000000-80-0-0.jpg",
+//         picture_big: "https://e-cdns-images.dzcdn.net/images/misc/6eca3188f724f04843a15e3e575751a5/500x500-000000-80-0-0.jpg",
+//         picture_xl: "https://e-cdns-images.dzcdn.net/images/misc/6eca3188f724f04843a15e3e575751a5/1000x1000-000000-80-0-0.jpg",
+//         type: "genre"
+//     },
+//     {
+//         name: "Jazz",
+//         id: 129,
+//         picture: "https://api.deezer.com/genre/129/image",
+//         picture_small: "https://e-cdns-images.dzcdn.net/images/misc/91468ecc5dfdd19c42a43d2cbdf27059/56x56-000000-80-0-0.jpg",
+//         picture_mediu: "https://e-cdns-images.dzcdn.net/images/misc/91468ecc5dfdd19c42a43d2cbdf27059/250x250-000000-80-0-0.jpg",
+//         picture_big: "https://e-cdns-images.dzcdn.net/images/misc/91468ecc5dfdd19c42a43d2cbdf27059/500x500-000000-80-0-0.jpg",
+//         picture_xl: "https://e-cdns-images.dzcdn.net/images/misc/91468ecc5dfdd19c42a43d2cbdf27059/1000x1000-000000-80-0-0.jpg",
+//         type: "genre"
+//     },
+//     {
+//         name: "Reggae",
+//         id: 144,
+//         picture: "https://api.deezer.com/genre/144/image",
+//         picture_small: "https://e-cdns-images.dzcdn.net/images/misc/7b901a98628cf879e1465f1dfd697e00/56x56-000000-80-0-0.jpg",
+//         picture_medium: "https://e-cdns-images.dzcdn.net/images/misc/7b901a98628cf879e1465f1dfd697e00/250x250-000000-80-0-0.jpg",
+//         picture_big: "https://e-cdns-images.dzcdn.net/images/misc/7b901a98628cf879e1465f1dfd697e00/500x500-000000-80-0-0.jpg",
+//         picture_xl: "https://e-cdns-images.dzcdn.net/images/misc/7b901a98628cf879e1465f1dfd697e00/1000x1000-000000-80-0-0.jpg",
+//         type: "genre"
+//     },
+//     {
+//         name: "Rap/Hip Hop",
+//         id: 116,
+//         picture: "https://api.deezer.com/genre/116/image",
+//         picture_small: "https://e-cdns-images.dzcdn.net/images/misc/5c27115d3b797954afff59199dad98d1/56x56-000000-80-0-0.jpg",
+//         picture_medium: "https://e-cdns-images.dzcdn.net/images/misc/5c27115d3b797954afff59199dad98d1/250x250-000000-80-0-0.jpg",
+//         picture_big: "https://e-cdns-images.dzcdn.net/images/misc/5c27115d3b797954afff59199dad98d1/500x500-000000-80-0-0.jpg",
+//         picture_xl: "https://e-cdns-images.dzcdn.net/images/misc/5c27115d3b797954afff59199dad98d1/1000x1000-000000-80-0-0.jpg",
+//         type: "genre"
+//     },
+//     {
+//         "id": "165",
+//         "name": "R&B",
+//         "picture": "https://api.deezer.com/genre/165/image",
+//         "picture_small": "https://e-cdns-images.dzcdn.net/images/misc/68a43aec844708e693cb99f47814153b/56x56-000000-80-0-0.jpg",
+//         "picture_medium": "https://e-cdns-images.dzcdn.net/images/misc/68a43aec844708e693cb99f47814153b/250x250-000000-80-0-0.jpg",
+//         "picture_big": "https://e-cdns-images.dzcdn.net/images/misc/68a43aec844708e693cb99f47814153b/500x500-000000-80-0-0.jpg",
+//         "picture_xl": "https://e-cdns-images.dzcdn.net/images/misc/68a43aec844708e693cb99f47814153b/1000x1000-000000-80-0-0.jpg",
+//         "type": "genre"
+//       },
+// ];
+
+
+// async function fetchData() {
+//     let artist = searchArtist;
+//     const url = `https://deezerdevs-deezer.p.rapidapi.com/artist/${artist}`;
+//     const options = {
+//         method: 'GET',
+//         headers: {
+//             'X-RapidAPI-Key': 'e20758a5acmsh7521840f2e5d171p17736bjsn049c16fce793',
+//             'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+//         }
+//     };
+
+//     try {
+//         const response = await fetch(url, options);
+//         const result = await response.json();
+//         return result
+//     } catch (error) {
+//         console.error(error);
+//     }
+// };
+
+
+// array.forEach(element => {
+    
+// });
+
+// function getData(data){
+// const name = document.createElement("p");
+// name.innerText= data.name
+// const pic = document.createElement("img");
+// pic.src= data.picture
+// view.appendChild(pic)
+// view.appendChild(name)
+// }
+// button.addEventListener("click",async (e) => {
+//     searchArtist = input.value
+//     e.preventDefault();
+//     const result = await fetchData();
+//  getData(result)
+
+// });
---
+++ javascript/start.js
@@ -0,0 +1,265 @@
+
+let array = [
+    {
+        name: "Rock",
+        id: 152,
+        picture: "https://api.deezer.com/genre/152/image",
+        picture_small: "https://e-cdns-images.dzcdn.net/images/misc/b36ca681666d617edd0dcb5ab389a6ac/56x56-000000-80-0-0.jpg",
+        picture_medium: "https://e-cdns-images.dzcdn.net/images/misc/b36ca681666d617edd0dcb5ab389a6ac/250x250-000000-80-0-0.jpg",
+        picture_big: "https://e-cdns-images.dzcdn.net/images/misc/b36ca681666d617edd0dcb5ab389a6ac/500x500-000000-80-0-0.jpg",
+        picture_xl: "https://e-cdns-images.dzcdn.net/images/misc/b36ca681666d617edd0dcb5ab389a6ac/1000x1000-000000-80-0-0.jpg",
+        type: "genre"
+    },
+    {
+        name: "Klassik",
+        id: 98,
+        picture: "https://api.deezer.com/genre/98/image",
+        picture_small: "https://e-cdns-images.dzcdn.net/images/misc/609f69b669b242252aa8ee09b5597655/56x56-000000-80-0-0.jpg",
+        picture_medium: "https://e-cdns-images.dzcdn.net/images/misc/609f69b669b242252aa8ee09b5597655/250x250-000000-80-0-0.jpg",
+        picture_big: "https://e-cdns-images.dzcdn.net/images/misc/609f69b669b242252aa8ee09b5597655/500x500-000000-80-0-0.jpg",
+        picture_xl: "https://e-cdns-images.dzcdn.net/images/misc/609f69b669b242252aa8ee09b5597655/1000x1000-000000-80-0-0.jpg",
+        type: "genre"
+    },
+    {
+        name: "Soul & Funk",
+        id: 169,
+        picture: "https://api.deezer.com/genre/169/image",
+        picture_small: "https://e-cdns-images.dzcdn.net/images/misc/3d5e8aab99b95bfa7ac7e9e466e7781e/56x56-000000-80-0-0.jpg",
+        picture_medium: "https://e-cdns-images.dzcdn.net/images/misc/3d5e8aab99b95bfa7ac7e9e466e7781e/250x250-000000-80-0-0.jpg",
+        picture_big: "https://e-cdns-images.dzcdn.net/images/misc/3d5e8aab99b95bfa7ac7e9e466e7781e/500x500-000000-80-0-0.jpg",
+        picture_xl: "https://e-cdns-images.dzcdn.net/images/misc/3d5e8aab99b95bfa7ac7e9e466e7781e/1000x1000-000000-80-0-0.jpg",
+        type: "genre"
+    },
+    {
+        name: "Country",
+        id: 84,
+        picture: "https://api.deezer.com/genre/84/image",
+        picture_small: "https://e-cdns-images.dzcdn.net/images/misc/6eca3188f724f04843a15e3e575751a5/56x56-000000-80-0-0.jpg",
+        picture_medium: "https://e-cdns-images.dzcdn.net/images/misc/6eca3188f724f04843a15e3e575751a5/250x250-000000-80-0-0.jpg",
+        picture_big: "https://e-cdns-images.dzcdn.net/images/misc/6eca3188f724f04843a15e3e575751a5/500x500-000000-80-0-0.jpg",
+        picture_xl: "https://e-cdns-images.dzcdn.net/images/misc/6eca3188f724f04843a15e3e575751a5/1000x1000-000000-80-0-0.jpg",
+        type: "genre"
+    },
+    {
+        name: "Jazz",
+        id: 129,
+        picture: "https://api.deezer.com/genre/129/image",
+        picture_small: "https://e-cdns-images.dzcdn.net/images/misc/91468ecc5dfdd19c42a43d2cbdf27059/56x56-000000-80-0-0.jpg",
+        picture_mediu: "https://e-cdns-images.dzcdn.net/images/misc/91468ecc5dfdd19c42a43d2cbdf27059/250x250-000000-80-0-0.jpg",
+        picture_big: "https://e-cdns-images.dzcdn.net/images/misc/91468ecc5dfdd19c42a43d2cbdf27059/500x500-000000-80-0-0.jpg",
+        picture_xl: "https://e-cdns-images.dzcdn.net/images/misc/91468ecc5dfdd19c42a43d2cbdf27059/1000x1000-000000-80-0-0.jpg",
+        type: "genre"
+    },
+    {
+        name: "Reggae",
+        id: 144,
+        picture: "https://api.deezer.com/genre/144/image",
+        picture_small: "https://e-cdns-images.dzcdn.net/images/misc/7b901a98628cf879e1465f1dfd697e00/56x56-000000-80-0-0.jpg",
+        picture_medium: "https://e-cdns-images.dzcdn.net/images/misc/7b901a98628cf879e1465f1dfd697e00/250x250-000000-80-0-0.jpg",
+        picture_big: "https://e-cdns-images.dzcdn.net/images/misc/7b901a98628cf879e1465f1dfd697e00/500x500-000000-80-0-0.jpg",
+        picture_xl: "https://e-cdns-images.dzcdn.net/images/misc/7b901a98628cf879e1465f1dfd697e00/1000x1000-000000-80-0-0.jpg",
+        type: "genre"
+    },
+    {
+        name: "Rap/Hip Hop",
+        id: 116,
+        picture: "https://api.deezer.com/genre/116/image",
+        picture_small: "https://e-cdns-images.dzcdn.net/images/misc/5c27115d3b797954afff59199dad98d1/56x56-000000-80-0-0.jpg",
+        picture_medium: "https://e-cdns-images.dzcdn.net/images/misc/5c27115d3b797954afff59199dad98d1/250x250-000000-80-0-0.jpg",
+        picture_big: "https://e-cdns-images.dzcdn.net/images/misc/5c27115d3b797954afff59199dad98d1/500x500-000000-80-0-0.jpg",
+        picture_xl: "https://e-cdns-images.dzcdn.net/images/misc/5c27115d3b797954afff59199dad98d1/1000x1000-000000-80-0-0.jpg",
+        type: "genre"
+    },
+    {
+        "id": "165",
+        "name": "R&B",
+        "picture": "https://api.deezer.com/genre/165/image",
+        "picture_small": "https://e-cdns-images.dzcdn.net/images/misc/68a43aec844708e693cb99f47814153b/56x56-000000-80-0-0.jpg",
+        "picture_medium": "https://e-cdns-images.dzcdn.net/images/misc/68a43aec844708e693cb99f47814153b/250x250-000000-80-0-0.jpg",
+        "picture_big": "https://e-cdns-images.dzcdn.net/images/misc/68a43aec844708e693cb99f47814153b/500x500-000000-80-0-0.jpg",
+        "picture_xl": "https://e-cdns-images.dzcdn.net/images/misc/68a43aec844708e693cb99f47814153b/1000x1000-000000-80-0-0.jpg",
+        "type": "genre"
+    },
+];
+
+array.forEach((genr) => {
+    const right = document.getElementById("boxRight")
+    right.style.display="flex";
+    right.style.flexWrap = "wrap"
+    right.style.gap = "20px"
+    console.log(right);
+    const div = document.createElement("div")
+    console.log(div);
+ const img= document.createElement("img")
+ console.log(img);
+ const genre= document.createElement("li")
+ console.log(genre);
+    img.src = genr.picture
+    genre.innerText = `${genr.name} `;
+
+    right.appendChild(div)
+    div.appendChild(img)
+    div.appendChild(genre)
+   
+   
+
+});
+
+// für 1 
+// const filterResult = array.filter((item)=> item.id == 0)
+// console.log(filterResult);
+
+
+const selectedGenre = array.find(item => item.name === "Alle");
+console.log(selectedGenre);
+const url = `https://deezerdevs-deezer.p.rapidapi.com/genre/${selectedGenre.name}`;
+console.log(url.length);
+console.log(url);
+
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'e20758a5acmsh7521840f2e5d171p17736bjsn049c16fce793',
+        'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+    }
+};
+console.log(options);
+
+(async () => {
+    try {
+        const response = await fetch(url, options);
+        console.log(response);
+        const result = await response.json();
+        console.log(result);
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+})();
+
+
+
+
+
+
+
+
+
+
+
+// const array = [
+//     {
+//                 name: "Rock",
+//                 id: 152,
+//                 picture: "https://api.deezer.com/genre/152/image",
+//                 picture_small: "https://e-cdns-images.dzcdn.net/images/misc/b36ca681666d617edd0dcb5ab389a6ac/56x56-000000-80-0-0.jpg",
+//                 picture_medium: "https://e-cdns-images.dzcdn.net/images/misc/b36ca681666d617edd0dcb5ab389a6ac/250x250-000000-80-0-0.jpg",
+//                 picture_big: "https://e-cdns-images.dzcdn.net/images/misc/b36ca681666d617edd0dcb5ab389a6ac/500x500-000000-80-0-0.jpg",
+//                 picture_xl: "https://e-cdns-images.dzcdn.net/images/misc/b36ca681666d617edd0dcb5ab389a6ac/1000x1000-000000-80-0-0.jpg",
+//                 type: "genre"
+//             },
+//             {
+//                 name: "Klassik",
+//                 id: 98,
+//                 picture: "https://api.deezer.com/genre/98/image",
+//                 picture_small: "https://e-cdns-images.dzcdn.net/images/misc/609f69b669b242252aa8ee09b5597655/56x56-000000-80-0-0.jpg",
+//                 picture_medium: "https://e-cdns-images.dzcdn.net/images/misc/609f69b669b242252aa8ee09b5597655/250x250-000000-80-0-0.jpg",
+//                 picture_big: "https://e-cdns-images.dzcdn.net/images/misc/609f69b669b242252aa8ee09b5597655/500x500-000000-80-0-0.jpg",
+//                 picture_xl: "https://e-cdns-images.dzcdn.net/images/misc/609f69b669b242252aa8ee09b5597655/1000x1000-000000-80-0-0.jpg",
+//                 type: "genre"
+//             },
+//             {
+//                 name: "Soul & Funk",
+//                 id: 169,
+//                 picture: "https://api.deezer.com/genre/169/image",
+//                 picture_small: "https://e-cdns-images.dzcdn.net/images/misc/3d5e8aab99b95bfa7ac7e9e466e7781e/56x56-000000-80-0-0.jpg",
+//                 picture_medium: "https://e-cdns-images.dzcdn.net/images/misc/3d5e8aab99b95bfa7ac7e9e466e7781e/250x250-000000-80-0-0.jpg",
+//                 picture_big: "https://e-cdns-images.dzcdn.net/images/misc/3d5e8aab99b95bfa7ac7e9e466e7781e/500x500-000000-80-0-0.jpg",
+//                 picture_xl: "https://e-cdns-images.dzcdn.net/images/misc/3d5e8aab99b95bfa7ac7e9e466e7781e/1000x1000-000000-80-0-0.jpg",
+//                 type: "genre"
+//             },
+//             {
+//                 name: "Country",
+//                 id: 84,
+//                 picture: "https://api.deezer.com/genre/84/image",
+//                 picture_small: "https://e-cdns-images.dzcdn.net/images/misc/6eca3188f724f04843a15e3e575751a5/56x56-000000-80-0-0.jpg",
+//                 picture_medium: "https://e-cdns-images.dzcdn.net/images/misc/6eca3188f724f04843a15e3e575751a5/250x250-000000-80-0-0.jpg",
+//                 picture_big: "https://e-cdns-images.dzcdn.net/images/misc/6eca3188f724f04843a15e3e575751a5/500x500-000000-80-0-0.jpg",
+//                 picture_xl: "https://e-cdns-images.dzcdn.net/images/misc/6eca3188f724f04843a15e3e575751a5/1000x1000-000000-80-0-0.jpg",
+//                 type: "genre"
+//             },
+//             {
+//                 name: "Jazz",
+//                 id: 129,
+//                 picture: "https://api.deezer.com/genre/129/image",
+//                 picture_small: "https://e-cdns-images.dzcdn.net/images/misc/91468ecc5dfdd19c42a43d2cbdf27059/56x56-000000-80-0-0.jpg",
+//                 picture_mediu: "https://e-cdns-images.dzcdn.net/images/misc/91468ecc5dfdd19c42a43d2cbdf27059/250x250-000000-80-0-0.jpg",
+//                 picture_big: "https://e-cdns-images.dzcdn.net/images/misc/91468ecc5dfdd19c42a43d2cbdf27059/500x500-000000-80-0-0.jpg",
+//                 picture_xl: "https://e-cdns-images.dzcdn.net/images/misc/91468ecc5dfdd19c42a43d2cbdf27059/1000x1000-000000-80-0-0.jpg",
+//                 type: "genre"
+//             },
+//             {
+//                 name: "Reggae",
+//                 id: 144,
+//                 picture: "https://api.deezer.com/genre/144/image",
+//                 picture_small: "https://e-cdns-images.dzcdn.net/images/misc/7b901a98628cf879e1465f1dfd697e00/56x56-000000-80-0-0.jpg",
+//                 picture_medium: "https://e-cdns-images.dzcdn.net/images/misc/7b901a98628cf879e1465f1dfd697e00/250x250-000000-80-0-0.jpg",
+//                 picture_big: "https://e-cdns-images.dzcdn.net/images/misc/7b901a98628cf879e1465f1dfd697e00/500x500-000000-80-0-0.jpg",
+//                 picture_xl: "https://e-cdns-images.dzcdn.net/images/misc/7b901a98628cf879e1465f1dfd697e00/1000x1000-000000-80-0-0.jpg",
+//                 type: "genre"
+//             },
+//             {
+//                 name: "Rap/Hip Hop",
+//                 id: 116,
+//                 picture: "https://api.deezer.com/genre/116/image",
+//                 picture_small: "https://e-cdns-images.dzcdn.net/images/misc/5c27115d3b797954afff59199dad98d1/56x56-000000-80-0-0.jpg",
+//                 picture_medium: "https://e-cdns-images.dzcdn.net/images/misc/5c27115d3b797954afff59199dad98d1/250x250-000000-80-0-0.jpg",
+//                 picture_big: "https://e-cdns-images.dzcdn.net/images/misc/5c27115d3b797954afff59199dad98d1/500x500-000000-80-0-0.jpg",
+//                 picture_xl: "https://e-cdns-images.dzcdn.net/images/misc/5c27115d3b797954afff59199dad98d1/1000x1000-000000-80-0-0.jpg",
+//                 type: "genre"
+//             },
+//             {
+//                 name: "Alle",
+//                 id: 0,
+//                 picture: "https://api.deezer.com/genre/0/image",
+//                 picture_small: "https://e-cdns-images.dzcdn.net/images/misc//56x56-000000-80-0-0.jpg",
+//                 picture_medium: "https://e-cdns-images.dzcdn.net/images/misc//250x250-000000-80-0-0.jpg",
+//                 picture_big: "https://e-cdns-images.dzcdn.net/images/misc//500x500-000000-80-0-0.jpg",
+//                 picture_xl: "https://e-cdns-images.dzcdn.net/images/misc//1000x1000-000000-80-0-0.jpg",
+//                 type: "genre"
+//             },
+// ];
+
+// array.forEach(genr => {
+//     const right = document.getElementById("boxRight");
+//     const genre = document.createElement("li");
+//     genre.innerText = `${genr.name} ${genr.id} ${genr.picture} ${genr.picture_small}`;
+//     right.appendChild(genre);
+// });
+
+// const selectedGenre = array.find(item => item.name === "Alle");
+
+// if (selectedGenre) {
+//     const url = `https://deezerdevs-deezer.p.rapidapi.com/genre/${selectedGenre.id}`;
+
+//     const options = {
+//         method: 'GET',
+//         headers: {
+//             'X-RapidAPI-Key': 'e20758a5acmsh7521840f2e5d171p17736bjsn049c16fce793',
+//             'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+//         }
+//     };
+
+//     (async () => {
+//         try {
+//             const response = await fetch(url, options);
+//             const result = await response.json();
+//             console.log(result);
+//         } catch (error) {
+//             console.error(error);
+//         }
+//     })();
+// } else {
+//     console.error("Genre 'Alle' not found in the array.");
+// }
+
