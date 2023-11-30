41b9bf4ad0d245bbd719d1beabd98676dcaef148
aselkaldd <kaldarbaeva.asel@gmail.com>
2023-11-30 23:08:18 +0100

    finished all tasks

--- javascript/pass.js
+++ javascript/pass.js
@@ -1,3 +1,44 @@
+
+// function checkPassword() {
+//     var password = document.getElementById("password").value;
+//     var errorText = document.getElementById("errorText");
+//     var passwordField = document.getElementById("password");
+
+//     if (password.length < 8) {
+//         errorText.style.display = "block";
+//         passwordField.style.border = "1px solid red";
+//     } else {
+//         errorText.style.display = "none";
+//         passwordField.style.border = "1px solid #ced4da";
+
+
+//         var dataToSave = { "password": password };
+
+
+//         var jsonData = JSON.stringify(dataToSave);
+
+
+//         fetch("save.json", {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//             body: jsonData,
+//         }).then(response => {
+
+//             if (response.ok) {
+
+//             } else {
+
+//             }
+//         }).catch(error => {
+
+//         });
+//     }
+// }
+
+
+
 function checkPassword() {
     var password = document.getElementById("password").value;
     var errorText = document.getElementById("errorText");
@@ -9,6 +50,9 @@ function checkPassword() {
     } else {
         errorText.style.display = "none";
         passwordField.style.border = "1px solid #ced4da"; 
+
+        localStorage.setItem("userPassword", password);
+        
         window.location.href = "http://127.0.0.1:5500/pages/form.html"; 
 }
 
--- javascript/registr.js
+++ javascript/registr.js
@@ -1,12 +1,27 @@
+// function checkEmail() {
+//   var email = document.getElementById("username").value;
+//   var errorText = document.getElementById("error");
+//   var emailField = document.getElementById("username");
+
+//   if (email.includes('@') && email.includes('.')) {
+//     var dataToSave = { "email": email };
+//     var jsonData = JSON.stringify(dataToSave);
+//     localStorage.setItem('userData', JSON.stringify(jsonData));
+//     window.location.href = "http://127.0.0.1:5500/pages/pass.html";
+//   } else {
+//     errorText.style.display = "block";
+//     emailField.style.border = "2px solid red";
+//   }
+// }
+
 function checkEmail() {
   var email = document.getElementById("username").value;
   var errorText = document.getElementById("error");
   var emailField = document.getElementById("username");
 
   if (email.includes('@') && email.includes('.')) {
-    var dataToSave = { "email": email };
-    var jsonData = JSON.stringify(dataToSave);
-    localStorage.setItem('userData', jsonData);
+    // var dataToSave = { "email": email };
+    localStorage.setItem('savedEmail', email); 
     window.location.href = "http://127.0.0.1:5500/pages/pass.html";
   } else {
     errorText.style.display = "block";
