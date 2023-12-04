e3288816da3c84bc9fa296c9b20aacd1794fbfd2
Tairou Mouhamed <tairou97-mouhamed@hotmail.com>
2023-12-04 12:43:49 +0100

    fast done

--- javascript/playlist.js
+++ javascript/playlist.js
@@ -1,15 +1,53 @@
+
+const contain = document.querySelector(".contain")
+contain.style.display = "flex"
+
 const playlist = document.querySelector(".playlist");
+playlist.style.display = "flex"
+playlist.style.height = "100vh"
+playlist.style.flexWrap = "wrap"
+playlist.style.backgroundImage = " linear-gradient(#272727 ,black)"
+
 console.log(playlist);
 const divP = document.createElement("div")
-console.log(divP);
-divP.style.display ="flex"
+divP.classList.add("divp")
+divP.style.display = "flex"
+divP.style.justifyContent = "end"
+divP.style.backgroundColor = " #121212"
+divP.style.flexWrap = "wrap"
+divP.style.width = "100%"
+divP.style.height = "7vh"
 
-console.log(playlist);
+const btn = document.querySelector(".btn")
+btn.style.margin = "25px 30px"
+btn.style.border = "none"
+btn.style.color = "black"
+btn.style.display = "flex"
+btn.style.justifyContent = "center"
+btn.style.alignItems = "center"
+btn.style.borderRadius = "50px"
+btn.style.padding = "0px 70px 0 70px"
 
+const registrieren = document.querySelector(".registrieren");
+registrieren.style.border = "none"
+registrieren.style.padding = "10px 30px"
+registrieren.style.margin = "25px 30px"
+registrieren.style.backgroundColor = " #121212"
+registrieren.style.color = "white"
+registrieren.style.borderRadius = "50px"
 
+const anmelden = document.querySelector(".anmelden")
+anmelden.style.border = "none"
+anmelden.style.padding = "10px 30px"
+anmelden.style.margin = "25px 30px"
+anmelden.style.borderRadius = "50px"
+divP.appendChild(btn)
+divP.appendChild(registrieren)
+divP.appendChild(anmelden)
+playlist.appendChild(divP)
 
 async function playlistSee() {
- // Fetch Rock
+    // Fetch Rock
     const url = 'https://deezerdevs-deezer.p.rapidapi.com/playlist/5';
     const options = {
         method: 'GET',
@@ -25,23 +63,48 @@ async function playlistSee() {
 
         console.log(result);
         // Box
-     
-    result.tracks.data.map((music)=>{
+        result.tracks.data.map((music) => {
+            const divCon = document.createElement("div");
+            divCon.classList.add("divCon")
+            divCon.style.display = "flex"
+            divCon.style.borderRadius = "7px"
+            divCon.style.width = "100%"
+            divCon.style.height = "10%"
+            divCon.style.cursor = "pointer"
+            divCon.style.margin = "10px"
+            const li = document.createElement("li")
+            li.style.listStyle = "none"
+            li.style.fontSize = " 14px"
+            li.style.height = "20px"
+            li.style.padding = "10px"
+            li.style.color = "white"
+            li.innerText = music.title
 
-        const li = document.createElement("li")
-    
-        li.innerText = music.title
-        const img =  document.createElement("img")
-       
-        img.src = music.album.cover
 
-  
-    playlist.appendChild(img)
-    playlist.appendChild(li)
+            const audio = document.createElement("audio")
+            audio.setAttribute("controls", "")
+
+            const source = document.createElement("source")
+            source.src = music.preview
+            source.type = "audio/mpeg"
+            source.style.border="2px solid red"
+           
+            const img = document.createElement("img")
+            img.style.borderRadius = "7px"
+            img.style.boxShadow = " 1px 6px 10px 1px #121212"
+            img.src = music.album.cover
+            playlist.appendChild(divCon)
+            playlist.appendChild(img)
+            playlist.appendChild(li)
+            divCon.appendChild(img)
+            divCon.appendChild(li)
+            divCon.appendChild(audio)
+            audio.appendChild(source)
 
 
-    })
-        
+
+        })
+
     } catch (error) {
         console.error(error);
     }
--- javascript/start.js
+++ javascript/start.js
@@ -8,7 +8,8 @@ let music = [
         picture_medium: "https://e-cdns-images.dzcdn.net/images/misc/b36ca681666d617edd0dcb5ab389a6ac/250x250-000000-80-0-0.jpg",
         picture_big: "https://e-cdns-images.dzcdn.net/images/misc/b36ca681666d617edd0dcb5ab389a6ac/500x500-000000-80-0-0.jpg",
         picture_xl: "https://e-cdns-images.dzcdn.net/images/misc/b36ca681666d617edd0dcb5ab389a6ac/1000x1000-000000-80-0-0.jpg",
-        type: "genre"
+        type: "genre",
+        
     },
     {
         name: "Klassik",
@@ -82,40 +83,47 @@ let music = [
         type: "genre"
     },
 ];
-
+// const regist = document.createElement("div")
+// regist.style.border = "1px solid red"
+// regist.style.height = "10vh"
+// regist.style.width = "70vw"
 
 // ForEach
 music.forEach((genr) => {
     const right = document.getElementById("boxRight");
     console.log(right);
     right.style.display = "flex";
+    right.style.justifyContent ="center"
     right.style.flexWrap = "wrap"
     right.style.gap = "20px"
     right.style.boxShadow = " 0px 100px 10px"
     right.style.backgroundColor = "rgb(19, 19, 19)"
-    right.style.backgroundImage = " linear-gradient(#303030 ,black)"
+    right.style.backgroundImage = " linear-gradient(#272727 ,black)"
+
+    
 
     //                   Div
     const div = document.createElement("div");
-    div.style.width = "190px"
-    div.style.minHeight = "30vh"
-    div.style.padding = "20px"
-    div.style.margin = "20px"
-    console.log(div);
-    div.style.background = " "
-    div.style.borderRadius = "5px"
+    div.style.textAlign ="center"
+    div.style.width ="150px"
+    div.style.height = "220px"
+    div.style.marginTop ="20px"
+    div.style.background = "#171717"
+    div.style.borderRadius = "7px"
 
     const playlist = document.createElement("a")
+    playlist.style.textDecoration = "none"
     playlist.href = "./playlist.html"
-
-
+    playlist.style.height ="250px"
 
     //                    Img
     const img = document.createElement("img")
     console.log(img);
-    img.style.height = "12rem"
-    img.style.width = "12rem"
-    img.style.padding = "10px"
+    img.style.marginTop = "10px";
+    img.style.height = "8rem"
+    img.style.width = " 8rem"
+    img.style.borderRadius = "7px"
+img.style.boxShadow = " 1px 6px 10px 1px #121212"
 
     //                      Genre
     const genre = document.createElement("li")
@@ -173,119 +181,5 @@ console.log(options);
 
 
 
-// const array = [
-//     {
-//                 name: "Rock",
-//                 id: 152,
-//                 picture: "https://api.deezer.com/genre/152/image",
-//                 picture_small: "https://e-cdns-images.dzcdn.net/images/misc/b36ca681666d617edd0dcb5ab389a6ac/56x56-000000-80-0-0.jpg",
-//                 picture_medium: "https://e-cdns-images.dzcdn.net/images/misc/b36ca681666d617edd0dcb5ab389a6ac/250x250-000000-80-0-0.jpg",
-//                 picture_big: "https://e-cdns-images.dzcdn.net/images/misc/b36ca681666d617edd0dcb5ab389a6ac/500x500-000000-80-0-0.jpg",
-//                 picture_xl: "https://e-cdns-images.dzcdn.net/images/misc/b36ca681666d617edd0dcb5ab389a6ac/1000x1000-000000-80-0-0.jpg",
-//                 type: "genre"
-//             },
-//             {
-//                 name: "Klassik",
-//                 id: 98,
-//                 picture: "https://api.deezer.com/genre/98/image",
-//                 picture_small: "https://e-cdns-images.dzcdn.net/images/misc/609f69b669b242252aa8ee09b5597655/56x56-000000-80-0-0.jpg",
-//                 picture_medium: "https://e-cdns-images.dzcdn.net/images/misc/609f69b669b242252aa8ee09b5597655/250x250-000000-80-0-0.jpg",
-//                 picture_big: "https://e-cdns-images.dzcdn.net/images/misc/609f69b669b242252aa8ee09b5597655/500x500-000000-80-0-0.jpg",
-//                 picture_xl: "https://e-cdns-images.dzcdn.net/images/misc/609f69b669b242252aa8ee09b5597655/1000x1000-000000-80-0-0.jpg",
-//                 type: "genre"
-//             },
-//             {
-//                 name: "Soul & Funk",
-//                 id: 169,
-//                 picture: "https://api.deezer.com/genre/169/image",
-//                 picture_small: "https://e-cdns-images.dzcdn.net/images/misc/3d5e8aab99b95bfa7ac7e9e466e7781e/56x56-000000-80-0-0.jpg",
-//                 picture_medium: "https://e-cdns-images.dzcdn.net/images/misc/3d5e8aab99b95bfa7ac7e9e466e7781e/250x250-000000-80-0-0.jpg",
-//                 picture_big: "https://e-cdns-images.dzcdn.net/images/misc/3d5e8aab99b95bfa7ac7e9e466e7781e/500x500-000000-80-0-0.jpg",
-//                 picture_xl: "https://e-cdns-images.dzcdn.net/images/misc/3d5e8aab99b95bfa7ac7e9e466e7781e/1000x1000-000000-80-0-0.jpg",
-//                 type: "genre"
-//             },
-//             {
-//                 name: "Country",
-//                 id: 84,
-//                 picture: "https://api.deezer.com/genre/84/image",
-//                 picture_small: "https://e-cdns-images.dzcdn.net/images/misc/6eca3188f724f04843a15e3e575751a5/56x56-000000-80-0-0.jpg",
-//                 picture_medium: "https://e-cdns-images.dzcdn.net/images/misc/6eca3188f724f04843a15e3e575751a5/250x250-000000-80-0-0.jpg",
-//                 picture_big: "https://e-cdns-images.dzcdn.net/images/misc/6eca3188f724f04843a15e3e575751a5/500x500-000000-80-0-0.jpg",
-//                 picture_xl: "https://e-cdns-images.dzcdn.net/images/misc/6eca3188f724f04843a15e3e575751a5/1000x1000-000000-80-0-0.jpg",
-//                 type: "genre"
-//             },
-//             {
-//                 name: "Jazz",
-//                 id: 129,
-//                 picture: "https://api.deezer.com/genre/129/image",
-//                 picture_small: "https://e-cdns-images.dzcdn.net/images/misc/91468ecc5dfdd19c42a43d2cbdf27059/56x56-000000-80-0-0.jpg",
-//                 picture_mediu: "https://e-cdns-images.dzcdn.net/images/misc/91468ecc5dfdd19c42a43d2cbdf27059/250x250-000000-80-0-0.jpg",
-//                 picture_big: "https://e-cdns-images.dzcdn.net/images/misc/91468ecc5dfdd19c42a43d2cbdf27059/500x500-000000-80-0-0.jpg",
-//                 picture_xl: "https://e-cdns-images.dzcdn.net/images/misc/91468ecc5dfdd19c42a43d2cbdf27059/1000x1000-000000-80-0-0.jpg",
-//                 type: "genre"
-//             },
-//             {
-//                 name: "Reggae",
-//                 id: 144,
-//                 picture: "https://api.deezer.com/genre/144/image",
-//                 picture_small: "https://e-cdns-images.dzcdn.net/images/misc/7b901a98628cf879e1465f1dfd697e00/56x56-000000-80-0-0.jpg",
-//                 picture_medium: "https://e-cdns-images.dzcdn.net/images/misc/7b901a98628cf879e1465f1dfd697e00/250x250-000000-80-0-0.jpg",
-//                 picture_big: "https://e-cdns-images.dzcdn.net/images/misc/7b901a98628cf879e1465f1dfd697e00/500x500-000000-80-0-0.jpg",
-//                 picture_xl: "https://e-cdns-images.dzcdn.net/images/misc/7b901a98628cf879e1465f1dfd697e00/1000x1000-000000-80-0-0.jpg",
-//                 type: "genre"
-//             },
-//             {
-//                 name: "Rap/Hip Hop",
-//                 id: 116,
-//                 picture: "https://api.deezer.com/genre/116/image",
-//                 picture_small: "https://e-cdns-images.dzcdn.net/images/misc/5c27115d3b797954afff59199dad98d1/56x56-000000-80-0-0.jpg",
-//                 picture_medium: "https://e-cdns-images.dzcdn.net/images/misc/5c27115d3b797954afff59199dad98d1/250x250-000000-80-0-0.jpg",
-//                 picture_big: "https://e-cdns-images.dzcdn.net/images/misc/5c27115d3b797954afff59199dad98d1/500x500-000000-80-0-0.jpg",
-//                 picture_xl: "https://e-cdns-images.dzcdn.net/images/misc/5c27115d3b797954afff59199dad98d1/1000x1000-000000-80-0-0.jpg",
-//                 type: "genre"
-//             },
-//             {
-//                 name: "Alle",
-//                 id: 0,
-//                 picture: "https://api.deezer.com/genre/0/image",
-//                 picture_small: "https://e-cdns-images.dzcdn.net/images/misc//56x56-000000-80-0-0.jpg",
-//                 picture_medium: "https://e-cdns-images.dzcdn.net/images/misc//250x250-000000-80-0-0.jpg",
-//                 picture_big: "https://e-cdns-images.dzcdn.net/images/misc//500x500-000000-80-0-0.jpg",
-//                 picture_xl: "https://e-cdns-images.dzcdn.net/images/misc//1000x1000-000000-80-0-0.jpg",
-//                 type: "genre"
-//             },
-// ];
 
-// array.forEach(genr => {
-//     const right = document.getElementById("boxRight");
-//     const genre = document.createElement("li");
-//     genre.innerText = `${genr.name} ${genr.id} ${genr.picture} ${genr.picture_small}`;
-//     right.appendChild(genre);
-// });
-
-// const selectedGenre = array.find(item => item.name === "Alle");
-
-// if (selectedGenre) {
-//     const url = `https://deezerdevs-deezer.p.rapidapi.com/genre/${selectedGenre.id}`;
-
-//     const options = {
-//         method: 'GET',
-//         headers: {
-//             'X-RapidAPI-Key': 'e20758a5acmsh7521840f2e5d171p17736bjsn049c16fce793',
-//             'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
-//         }
-//     };
-
-//     (async () => {
-//         try {
-//             const response = await fetch(url, options);
-//             const result = await response.json();
-//             console.log(result);
-//         } catch (error) {
-//             console.error(error);
-//         }
-//     })();
-// } else {
-//     console.error("Genre 'Alle' not found in the array.");
-// }
 
